f660ee7d1a8d6edab740bcbfa50ad0a64ea541eb
Shruti <shruti.sachdeva@galgotiasuniversity.edu.in>
2025-12-20 14:57:34 +0530

    New Project Commit

---
+++ Projects/CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Shruti Sachdeva on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
