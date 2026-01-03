4c3b172dfd8631f7d83290584ba3a5541154a862
Shruti <shruti.sachdeva@galgotiasuniversity.edu.in>
2026-01-03 13:23:38 +0530

    CodeWordBreaker Project created

---
+++ Projects/CodeWordBreaker/CodeWordBreaker/CodeWordBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWordBreakerApp.swift
+//  CodeWordBreaker
+//
+//  Created by Shruti Sachdeva on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeWordBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
