3c67c1a039af95dc51bb914b686a2c8b299abba4
Shruti <shruti.sachdeva@galgotiasuniversity.edu.in>
2026-01-31 23:13:26 +0530

    L13 code breaker project provided by instructor
    L13 project by instructor

---
+++ Projects/Projects provided by instructor/CodeBreakerL13/CodeBreaker/Model/CodeBreaker.swift
@@ -0,0 +1,75 @@
+//
+//  CodeBreaker.swift
+//  CodeBreaker
+//
+//  Created by CS193p Instructor on 4/9/25.
+//
+
+import Foundation
+import SwiftData
+
+typealias Peg = String
+
+@Model class CodeBreaker {
+    var name: String
+    @Relationship(deleteRule: .cascade) var masterCode: Code = Code(kind: .master(isHidden: true))
+    @Relationship(deleteRule: .cascade) var guess: Code = Code(kind: .guess)
+    @Relationship(deleteRule: .cascade) var attempts: [Code] = []
+    var pegChoices: [Peg]
+    @Transient var startTime: Date?
+    var endTime: Date?
+    var elapsedTime: TimeInterval = 0
+    
+    init(name: String = "Code Breaker", pegChoices: [Peg]) {
+        self.name = name
+        self.pegChoices = pegChoices
+        masterCode.randomize(from: pegChoices)
+    }
+    
+    func startTimer() {
+        if startTime == nil, !isOver {
+            startTime = .now
+        }
+    }
+    
+    func pauseTimer() {
+        if let startTime {
+            elapsedTime += Date.now.timeIntervalSince(startTime)
+        }
+        startTime = nil
+    }
+    
+    var isOver: Bool {
+        attempts.first?.pegs == masterCode.pegs
+    }
+    
+    func restart() {
+        masterCode.kind = .master(isHidden: true)
+        masterCode.randomize(from: pegChoices)
+        guess.reset()
+        attempts.removeAll()
+        startTime = .now
+        endTime = nil
+        elapsedTime = 0
+    }
+    
+    func attemptGuess() {
+        guard !attempts.contains(where: { $0.pegs == guess.pegs }) else { return }
+        let attempt = Code(
+            kind: .attempt(guess.match(against: masterCode)),
+            pegs: guess.pegs
+        )
+        attempts.insert(attempt, at: 0)
+        guess.reset()
+        if isOver {
+            endTime = .now
+            masterCode.kind = .master(isHidden: false)
+            pauseTimer()
+        }
+    }
+    
+    func setGuessPeg(_ peg: Peg, at index: Int) {
+        guard guess.pegs.indices.contains(index) else { return }
+        guess.pegs[index] = peg
+    }
+}
